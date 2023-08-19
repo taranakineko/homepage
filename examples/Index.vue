2d71210faf5452aa005fd82a6a9f853e16b355ff
taranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2023-08-19 18:47:48 +0800

    a little edit()

--- examples/Index.vue
+++ examples/Index.vue
@@ -27,7 +27,7 @@
                 </ss-card>
                 <ss-card title="名字从何而来？😮" icon style="margin: 20px 5px 0px 5px;">
                     <p>TaranakiNeko 这个名字其实是别人<!-- 在此感谢 Moe23333 起的名 -->起的（当时只有 Taranaki）</p>
-                    <p>随后 Tara 自己擅自主张的加了 Neko，于是就有了现在这个名字</p>
+                    <p>随后 Tara 自己擅自主张的加上了 Neko，于是就有了现在这个名字</p>
                     <p>至于 「千畔」嘛... 「沉舟侧畔千帆过，病树前头万木春」</p>
                 </ss-card>
                 <ss-card title="那其他的呢？👀" icon style="margin: 20px 5px 0px 5px;">
