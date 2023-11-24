71ecab1045f58cbff9dd8a69eb223a2f6eec31d1
taranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2023-11-24 21:00:32 +0800

    rewrite 404

--- src/components/FourOhFour.vue
+++ src/components/FourOhFour.vue
@@ -1,36 +1,38 @@
 <template>
-    <div class="miao">
-        <div class="fof">
-            <div class="top"></div>
-            <div class="title">404</div>
-            <div class="sub">这个页面不见了</div>
-            <div class="bottom"></div>
-        </div>
+    <div class="miao-fof">
+        <br><br>    
+        <mdui-circular-progress></mdui-circular-progress>
+        <h1>哦，我的上帝...！</h1>
+        <p>真是不敢相信，你居然会找到这里，这简直就像萨莉舅妈烤糊了的苹果派一样糟糕！</p>
+        <p>我发誓，如果你尝试一下其他地方，你准会回到正轨的！</p>
     </div>
 </template>
 
 <style lang="sass">
-.fof
-    display: flex
-    flex-direction: column
-    gap: 5px
-    height: 50vh
+mdui-circular-progress
+    scale: 150%
+    margin-bottom: 3.14159%
+    margin-left: 1.5%
+
+.miao-fof
+    max-width: 667px
+    min-width: 360px
+    padding-right: 0px
+    padding-left: 0px
+    padding-bottom: 1%
+    padding-top: 1%
+    // text-align: center
+    position: absolute
+    margin: auto
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    width: 60%
+    height: 50%
 
 .top
     flex-grow: 1
-
-.title
-    font-size: min(15em, 40vw)
-    color: transparent
-    background-color: rgb(68 140 188 / 50%)
-    -webkit-background-clip: text
-    text-shadow: rgba(255, 255, 255, 0.5) 0 5px 6px
-    text-align: center
-
-.sub
-    color: rgba(0, 0, 0, 0.33)
-    font-size: 1.2em
-    text-align: center
 
 .bottom
     flex-grow: 2
