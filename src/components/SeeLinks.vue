1b67a96b4674c1d069c375715775b5047a1c4541
taranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2023-11-30 20:33:44 +0800

    edit links
      - links-right 高度修改
      - add link sunyz + others
      - 所有链接将以新标签页打开

--- src/components/SeeLinks.vue
+++ src/components/SeeLinks.vue
@@ -14,6 +14,7 @@
                     </mdui-list-item>
                     <mdui-list-item
                         href="https://alive.bar/@TaranakiNeko"
+                        target="_blank"
                         description="@TaranakiNeko@alive.bar"
                         end-icon="open_in_new"
                     >
@@ -21,6 +22,7 @@
                     </mdui-list-item>
                     <mdui-list-item
                         href="https://twitter.com/taranakineko"
+                        target="_blank"
                         description="@taranakineko"
                         end-icon="open_in_new"
                     >
@@ -28,6 +30,7 @@
                     </mdui-list-item>
                     <mdui-list-item
                         href="https://t.me/taranaki_neko"
+                        target="_blank"
                         description="Tara - 千畔 换乘站"
                         end-icon="open_in_new"
                     >
@@ -35,6 +38,7 @@
                     </mdui-list-item>
                     <mdui-list-item
                         href="https://github.com/taranakineko"
+                        target="_blank"
                         description="@taranakineko"
                         end-icon="open_in_new"
                     >
@@ -42,6 +46,7 @@
                     </mdui-list-item>
                     <mdui-list-item
                         href="https://instagram.com/taranakineko"
+                        target="_blank"
                         description="@taranakineko"
                         end-icon="open_in_new"
                     >
@@ -56,6 +61,7 @@
                         <div style="margin-left: 2.5rem">
                             <mdui-list-item
                                 href="https://bookmarks.nekoq.top/"
+                                target="_blank"
                                 description="看看千畔都看了什么"
                                 icon="bookmark"
                                 end-icon="open_in_new"
@@ -64,6 +70,7 @@
                             </mdui-list-item>
                             <mdui-list-item
                                 href="https://github.com/users/taranakineko/projects/1/views/1"
+                                target="_blank"
                                 description="填坑计划！"
                                 icon="checklist"
                                 end-icon="open_in_new"
@@ -78,23 +85,47 @@
         <mdui-card variant="elevated" class="link-right">
             <h2 style="margin-bottom: 10px">这是友链！🫂</h2>
             <mdui-list>
-                <mdui-list-item href="https://moe23333.vercel.app/" end-icon="open_in_new">
+                <mdui-list-item
+                    href="https://moe23333.vercel.app/"
+                    target="_blank"
+                    end-icon="open_in_new"
+                >
                     <mdui-avatar
                         slot="icon"
                         src="https://moe23333.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_trans.1f5053b1.png&w=1920&q=75"
                     ></mdui-avatar>
                     Moe23333
                 </mdui-list-item>
-                <mdui-list-item href="https://winsloweric.com/" end-icon="open_in_new">
+                <mdui-list-item
+                    href="https://winsloweric.com/"
+                    target="_blank"
+                    end-icon="open_in_new"
+                >
                     <mdui-avatar
                         slot="icon"
                         src="https://winsloweric.com/assets/logo256t.4b80bfe0.png"
                     ></mdui-avatar>
                     Pivot of Winslow
                 </mdui-list-item>
-                <mdui-list-item href="https://shef.cc/" end-icon="open_in_new"
+                <mdui-list-item href="https://shef.cc/" target="_blank" end-icon="open_in_new"
                     ><mdui-avatar slot="icon" src="https://shef.cc/logo.png"></mdui-avatar>
-                    SheepChef Blog
+                    SheepChef
+                </mdui-list-item>
+                <mdui-list-item href="https://sunyz.net/" target="_blank" end-icon="open_in_new"
+                    ><mdui-avatar
+                        slot="icon"
+                        src="https://cdn.sunyz.net/avatar@128x128.webp"
+                    ></mdui-avatar>
+                    Sunyz
+                </mdui-list-item>
+                <mdui-list-item
+                    href="https://blog.nekoq.top/links"
+                    target="_blank"
+                    description="will open new website"
+                    end-icon="open_in_new"
+                >
+                    Motto motto...
+                    <!-- https://www.youtube.com/watch?v=7uiKC1P7__U -->
                 </mdui-list-item>
             </mdui-list>
         </mdui-card>
