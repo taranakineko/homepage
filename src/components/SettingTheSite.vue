9d4c14b2fec243c656def1546653b0db366c76a5
taranakiNeko <55310629+taranakiNeko@users.noreply.github.com>
2024-01-06 00:56:25 +0800

    写不下去了.jpg

--- src/components/SettingTheSite.vue
+++ src/components/SettingTheSite.vue
@@ -5,10 +5,10 @@
     </mdui-top-app-bar>
     <div class="miao">
         <div>
-            <p>语言设置</p>
-            <mdui-radio-group value="chinese" ref="lang" id="lang">
-                <mdui-radio value="chinese">中文</mdui-radio>
-                <mdui-radio value="zh-ms">zh-MS</mdui-radio>
+            <p>{{ $t('setting_lanage') }}</p>
+            <mdui-radio-group ref="lang" id="lang">
+                <mdui-radio value="zh-CN">中文</mdui-radio>
+                <mdui-radio value="zh-MS">zh-MS</mdui-radio>
             </mdui-radio-group>
             <p v-html="la"></p>
         </div>
@@ -16,19 +16,27 @@
 </template>
 
 <script setup lang="ts">
+import app from '@/main'
 import { ref, onMounted } from 'vue'
 const lang = ref<any>(null)
 console.log(localStorage.getItem("lanauage"))
 const la = localStorage.getItem("lanauage") // 调试用
 const lanauage = localStorage.getItem("lanauage")
 onMounted(() => {
+    console.log(app.config.globalProperties.$i18n.locale) // 调试用
     console.log(lang.value.value) // 调试用
+
     if (localStorage.getItem("lanauage")) { // 判断是否有 lanauage，感觉有点多此一举
         lang.value.value = lanauage
+    } else {
+        lang.value.value = 'zh-CN'
     }
+
     lang.value.addEventListener('click', () => {
         console.log(lang.value.value) // 调试用
         localStorage.setItem("lanauage", lang.value.value)
+        app.config.globalProperties.$i18n.locale = lang.value.value
+        console.log(app.config.globalProperties.$i18n.locale) // 调试用
     })
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+
 import App from './App.vue'
 import router from './router'
+
+import zh from './assets/lan/zh-CN.json'
+import zh_ms from './assets/lan/zh-CN.json'
+
 import './assets/main.css'
 import 'mdui/mdui.css'
+
 import 'mdui'
+
 import { setColorScheme } from 'mdui/functions/setColorScheme.js'
 setColorScheme('#AEC9D0')
+
+// 载入 l10n
+const messages = {
+    'zh-CN': zh,
+    'zh-MS': zh_ms
+}
+// 初始化 i18n
+export const i18n = createI18n({
+  legacy: false,
+  locale: 'zh-CN',
+  fallbackLocale: 'zh-CN',
+  silentFallbackWarn: true,
+  messages
+})
+
 const app = createApp(App)
 
+app.use(i18n)
 app.use(router)
 app.mount('#app')
+export default app
